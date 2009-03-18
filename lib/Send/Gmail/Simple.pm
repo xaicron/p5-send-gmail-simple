bb7f1f968431e9b252f054a937e912e94787a8d2
xaicron <xaicron@gmail.com>
2009-03-19 00:47:05 +0900

    initial commit

---
+++ lib/Send/Gmail/Simple.pm
@@ -0,0 +1,33 @@
+package Send::Gmail::Simple;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Send::Gmail::Simple -
+
+=head1 SYNOPSIS
+
+  use Send::Gmail::Simple;
+
+=head1 DESCRIPTION
+
+Send::Gmail::Simple is
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
